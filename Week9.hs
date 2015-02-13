f84ad30df94748b905036dc430cc6656db7a365e
Andrew Stanton <up683693@myport.ac.uk>
2015-02-13 22:40:52 +0000

    Tried IO. Tried

--- Week9.hs
+++ Week9.hs
@@ -8,3 +8,9 @@ wordsToString (x:xs) = x ++ "\n" ++ wordsToString xs
 wordsOfLength :: Int -> [String] -> [String]
 -- wordsOfLength givenLength listIn = filter (givenLength == length ) listIn 
 wordsOfLength givenLength listIn = [word | word <- listIn, length word == givenLength]
+
+--main :: IO ()
+main = do
+    content <- readFile "words.txt"    -- This is a string?
+    --putStrLn content
+    read (return (content) :: [String]) -- return to get IO, which is what read wants for input???
