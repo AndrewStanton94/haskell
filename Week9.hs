4691625d92bb8d8dd87e6f0801551efda690c39d
AndrewStanton94 <up683693@myport.ac.uk>
2015-02-19 09:51:41 +0000

    Need to do menu

--- Week9.hs
+++ Week9.hs
@@ -17,9 +17,20 @@ main = do
     let strData = wordsToString (listData2)     -- (String)
     putStrLn (strData)
     writeFile "words.txt" (show listData2)
-    --putStrLn (show (addWord "Lemon" info))
-    --putStrLn (show (length info))
 
 -- return: cast to IO
 -- read String :: type: cast
 -- show var: cast to String
+
+--menu = do
+	
+
+
+--	h(f(g(a)))
+--	=
+--	h $ f $ g a
+
+-- http://learnhaskell.blogspot.co.uk/2007/09/lesson-3-case-3.html
+-- http://zvon.org/other/haskell/Outputsyntax/caseQexpressions_reference.html
+-- http://stackoverflow.com/questions/940382/haskell-difference-between-dot-and-dollar-sign
+-- http://blog.ezyang.com/2011/11/how-to-read-haskell/
